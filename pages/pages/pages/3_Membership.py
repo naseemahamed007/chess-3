abe0aa492cfa98b16ab47b9b2140667d39fb4cf5
naseemahamed007 <naseemishere0@gmail.com>
2026-03-05 03:36:46 +0530

    Create 3_Membership.py

---
+++ pages/pages/pages/3_Membership.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("👥 Become a Member")
+
+st.write("""
+Join Nas Matrix Chess and become
+part of our professional academy.
+""")
+
+
+st.link_button(
+    "Join Membership",
+    "PASTE_MEMBERSHIP_FORM_LINK",
+    use_container_width=True
+)
