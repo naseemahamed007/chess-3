b9d25a8a4105a7042fec39bf8195ae833c26ba46
naseemahamed007 <naseemishere0@gmail.com>
2026-03-05 03:49:47 +0530

    Update 2_Tournaments.py

--- pages/pages/2_Tournaments.py
+++ pages/pages/2_Tournaments.py
@@ -1,19 +1,20 @@
 import streamlit as st
 
-st.title("🏆 Tournaments")
-
-st.subheader("Upcoming Event")
+st.title("🏆 Chennai Open Chess Championship")
 
 st.write("""
-### Chennai Open Chess Championship
 Date: 28 March 2026  
 Mode: Online  
-Platform: Chess.com
+Platform: Chess.com  
+Format: Rapid
 """)
 
+st.divider()
+
+st.subheader("📝 Register Now")
 
 st.link_button(
-    "📝 Register Now",
-    "PASTE_YOUR_GOOGLE_FORM_LINK",
+    "Open Registration Form",
+    "https://docs.google.com/forms/d/e/1FAIpQLSd8FKhx8SFzMnJxrstoErtyNfKMJm92otwC7DTpr3LsaFmCnw/viewform?usp=sharing&ouid=116092041594100890090",
     use_container_width=True
 )
